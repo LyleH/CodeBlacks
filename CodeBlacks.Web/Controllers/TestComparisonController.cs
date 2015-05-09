0560823091dc931564a5f7be4d87d65315fbe8b3
jeremybeavon <jeremy.beavon@users.noreply.github.com>
2015-05-09 00:35:30 +0000

    Retrieve file from Azure storage

---
+++ CodeBlacks.Web/Controllers/TestComparisonController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace CodeBlacks.Web.Controllers
+{
+    public class TestComparisonController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public HttpResponseMessage Get(string id)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["TestComparisonStorage"].ToString());
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer testComparisonContainer = blobClient.GetContainerReference("testcomparisons");
+            CloudBlockBlob blob = testComparisonContainer.GetBlockBlobReference(id + ".json");
+            string text = blob.DownloadText();
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(text, Encoding.UTF8, "application/json");
+            return response;
+        }
+        
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
