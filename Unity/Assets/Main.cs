f321079b3125b8154352f2a05a4c81427f14ccb0
andrewHawken <ajhawken@gmail.com>
2015-05-09 16:26:24 +1200

    updating gui

---
+++ Unity/Assets/Main.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+
+		GameObject obj = Instantiate(canvas) as GameObject;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
